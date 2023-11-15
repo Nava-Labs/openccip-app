35ba83223fc96e2767f9b2a0af9fb225fe78ac03
jonassunandar <jonassunandar@yahoo.com>
2023-11-15 19:58:49 +0700

    trigger vercel

--- app/api/best-routes/route.tsx
+++ app/api/best-routes/route.tsx
@@ -121,7 +121,7 @@ function calculateBestRoutes(from:any, to:any){
 
 
 export async function GET(request: Request) {
-
+ 
   const { searchParams } = new URL(request.url)
   const from = searchParams.get('from')
   const to = searchParams.get('to')
