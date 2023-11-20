1aea7c60adcd1855e8f306283c9b463a1ae794be
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-11-20 18:42:35 +0700

    fix: updated landing page copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,53 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <h2 className={`mb-3 text-2xl font-semibold`}>Docs </h2>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-left">
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            How we build Crosslik?{" "}
+          </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            <ul className="list-disc">
+              <li>
+                Chainlink CCIP: allows the seamless cross-chain transfer and
+                balance data syncing of these extended tokens used for payment.
+              </li>
+              <li>
+                The Graph: Index data such as the details of listed NFTs and
+                ease the process of showing data in the front end.
+              </li>
+              <li>
+                Djikstra&apos;s Algorithm: Built into an SDK. It helps finding
+                the best route between chains without direct lane and
+                interacting with smart contract.
+              </li>
+            </ul>
           </p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <h2 className={`mb-3 text-2xl font-semibold`}>Learn </h2>
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-center">
+          <h2 className={`mb-3 text-2xl font-semibold`}>What is Crosslink? </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Crosslink aids developers in integrating cross-chain functionality
+            into new or existing dApps, enabling seamless transactions across
+            different blockchains to improve user experience. We&apos;ll be
+            showcasing its utility in an NFT marketplace, where users could buy
+            NFTs on one chain using funds from another, exemplifying
+            Crosslink&apos;s versatility in practical applications.
           </p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <h2 className={`mb-3 text-2xl font-semibold`}>Templates </h2>
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-right">
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Why we build Crosslink?{" "}
+          </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </div>
-
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <h2 className={`mb-3 text-2xl font-semibold`}>Deploy </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            We want to remove the frictions users experience when moving funds
+            across various chains. The most pressing issue is finding a bridge
+            protocol that supports both the source and destination chains,
+            aligns with the asset being transferred, and provides ample
+            liquidity for the transaction. Compounding these challenges are the
+            security risks.
           </p>
         </div>
       </div>
