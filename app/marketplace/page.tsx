4693b1f04b3f96414c045a8819bfd82b2e8d208f
keivnjs <kjsurtan@gmail.com>
2023-11-24 18:29:32 +0700

    fix: add grids into layout marketplace

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -4,6 +4,7 @@ import Image from "./modules/Image";
 import { getNftsQuery } from "@/lib/gql/queries/nft";
 import { Client, cacheExchange, fetchExchange } from "@urql/core";
 import Link from "next/link";
+import { chainList } from "@/lib/config/chain";
 
 const client = new Client({
   url: "https://api.thegraph.com/subgraphs/name/erwinphanglius/crosslink-subgraph",
@@ -17,34 +18,44 @@ export default async function Marketplace() {
   let nfts = nftsRes.data?.listedNFTs.filter((item) => item.price !== "0");
 
   return (
-    <>
-      {nfts?.map((item: any) => (
-        <Link key={item.id} href={`./marketplace/${item.id}`}>
-          <div className="flex flex-col">
-            <div className="relative overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-              <Image uri={item.uri} />
-            </div>
-            <div className="px-2 py-5 space-y-3">
-              <span className="">
-                {item.collectionName} # {item.id.split("-")[1]}
-              </span>
-              <div className="flex border px-5 py-2 bg-neutral-900 rounded-xl justify-between space-x-7">
-                <div className="flex flex-col gap-4">
-                  <span className="">Price</span>
-                  {Number(BigInt(item.price) / BigInt(1e18))}
-                </div>
-                <div className="flex flex-col gap-4">
-                  <span>Chain</span>
-                  <img
-                    src="https://cryptologos.cc/logos/apecoin-ape-ape-logo.svg?v=026"
-                    className="h-5 items-center justify-center"
-                  />
+    <div className="grid grid-cols-4 gap-10">
+      {nfts?.map((item: any) => {
+        const chainInfo = chainList.find(
+          (chain) => chain.chainId === item.chainOrigin
+        );
+
+        return (
+          <Link key={item.id} href={`./marketplace/${item.id}`}>
+            <div className="flex flex-col border border-neutral-500 rounded-xl">
+              <div className="relative  overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+                <Image uri={item.uri} />
+              </div>
+              <div className="px-2 py-5 space-y-3 whitespace-nowrap">
+                <span className="">
+                  {item.collectionName} # {item.id.split("-")[1]}
+                </span>
+                <div className="flex border px-5 py-2 bg-neutral-900 rounded-xl justify-between space-x-7">
+                  <div className="flex flex-col gap-4">
+                    <span className="">Price</span>
+                    {Number(BigInt(item.price) / BigInt(1e18))}
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <span>Chain</span>
+                    {chainInfo ? (
+                      <img
+                        src={chainInfo.logo}
+                        className="h-5 items-center justify-center"
+                      />
+                    ) : (
+                      <span>Chain information not found</span>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Link>
-      ))}
-    </>
+          </Link>
+        );
+      })}
+    </div>
   );
 }
