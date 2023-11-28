a445a52cafa6c996e3cc15c05bf17d92d8e168c1
keivnjs <kjsurtan@gmail.com>
2023-11-28 15:32:52 +0700

    fix: rename chainId with chainSelector

--- lib/config/chain.ts
+++ lib/config/chain.ts
@@ -1,43 +1,51 @@
 export interface Chain {
-  chainId: string;
+  chainSelector: string;
   name: string;
   logo: string;
+  slug: string;
 }
 
 export const chainList: Chain[] = [
   {
-    chainId: "16015286601757825753",
+    chainSelector: "16015286601757825753",
     name: "Ethereum Sepolia",
     logo: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=029",
+    slug: "sepolia-testnet",
   },
   {
-    chainId: "2664363617261496610",
+    chainSelector: "2664363617261496610",
     name: "Optimism Goerli",
     logo: "https://cryptologos.cc/logos/optimism-ethereum-op-logo.svg?v=029",
+    slug: "op-testnet",
   },
   {
-    chainId: "14767482510784806043",
+    chainSelector: "14767482510784806043",
     name: "Avalanche Fuji",
     logo: "https://cryptologos.cc/logos/avalanche-avax-logo.svg?v=029",
+    slug: "fuji-testnet",
   },
   {
-    chainId: "6101244977088475029",
+    chainSelector: "6101244977088475029",
     name: "Arbitrum Testnet",
     logo: "https://cryptologos.cc/logos/arbitrum-arb-logo.svg?v=029",
+    slug: "arbitrum-testnet",
   },
   {
-    chainId: "12532609583862916517",
+    chainSelector: "12532609583862916517",
     name: "Polygon Mumbai",
     logo: "https://cryptologos.cc/logos/polygon-matic-logo.svg?v=029",
+    slug: "polygon-testnet",
   },
   {
-    chainId: "5790810961207155433",
+    chainSelector: "5790810961207155433",
     name: "Base Goerli",
     logo: "https://s3.ap-southeast-1.amazonaws.com/mesprotocol.com/mes_images/chain_logos/base_logo.svg",
+    slug: "base-testnet",
   },
   {
-    chainId: "13264668187771770619",
+    chainSelector: "13264668187771770619",
     name: "BSC Testnet",
     logo: "https://cryptologos.cc/logos/binance-usd-busd-logo.svg?v=029",
+    slug: "bsc-testnet",
   },
 ];
