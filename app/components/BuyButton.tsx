3acaa479de4f06ce1febf96cb00315411096dc25
keivnjs <kjsurtan@gmail.com>
2023-11-28 21:58:34 +0700

    fix: add show bestRoutes

--- app/components/BuyButton.tsx
+++ app/components/BuyButton.tsx
@@ -4,120 +4,127 @@ import { useState } from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { handleSendTx } from "@/lib/modules/hooks/handleSendTx";
+import { chainList } from "@/lib/config/chain";
+import { createWalletClient, custom } from "viem";
+import { polygonMumbai, sepolia } from "viem/chains";
+import { Client, cacheExchange, fetchExchange } from "@urql/core";
+import { getNftDetailsQuery } from "@/lib/gql/queries/nft";
+const { OpenCCIP } = require("openccip-sdk");
 
 type Props = {
   tokenAddress: string;
   tokenId: string;
 };
 
-export function BuyButton({ tokenAddress, tokenId }: Props) {
+export default function BuyButton({ tokenAddress, tokenId }: Props) {
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedChain, setSelectedChain] = useState("Polygon");
-  const [selectedChainId, setSelectedChainId] = useState(80001);
+  const [selectedChain, setSelectedChain] = useState("");
+  const [selectedChainId, setSelectedChainId] = useState("");
+  const [bestRoutes, setBestRoutes] = useState<any[]>([]);
+  const [showRoutes, setShowRoutes] = useState(false);
 
-  const handleChainSelect = (chain: string, chainId: number) => {
+  const handleChainSelect = async (chain: string, chainId: string) => {
+    const selectedChainDetails = chainList.find((item) => item.name === chain);
+    const selectedChainSlug = selectedChainDetails
+      ? selectedChainDetails.slug
+      : "";
+
     setSelectedChain(chain);
     setSelectedChainId(chainId);
+
+    try {
+      await fetchData(selectedChainSlug);
+      setShowRoutes(true);
+    } catch (error) {
+      console.error("Error getting best route:", error);
+    }
   };
+
+  const fetchData = async (FROM: string) => {
+    const selectedChainDetails = chainList.find((item) => item.slug === FROM);
+
+    const TO = "polygon-testnet";
+
+    const account = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    const client = createWalletClient({
+      chain: polygonMumbai,
+      account,
+      transport: custom(window.ethereum),
+    });
+
+    const openccip = new OpenCCIP(client);
+
+    const bestRoutes = await openccip.fetchBestRoutes(FROM, TO);
+    setBestRoutes(bestRoutes.data);
+  };
+
+  console.log("Best Route:", bestRoutes);
 
   return (
     <>
-      <div className="w-full text-center ">
-        <button
-          disabled={isLoading}
-          onClick={async () => {
-            setIsLoading(true);
+      <div className="flex items-center rounded-xl bg-blue-500 py-2">
+        <div className="w-full text-center">
+          <button
+            disabled={isLoading}
+            onClick={async () => {
+              setIsLoading(true);
+              const result = await handleSendTx(tokenAddress);
+              console.log(result);
+            }}
+            className="text-center flex-1 w-full"
+          >
+            {isLoading ? "Loading..." : "Buy"}
+          </button>
+        </div>
 
-            const result = await handleSendTx();
-            console.log(result);
-          }}
-          className="text-center flex-1 w-full"
-        >
-          {isLoading ? "Loading..." : "Buy"}
-        </button>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger className="flex space-x-2 items-center pl-5 pr-2 bg-blue-500 border-l-2">
+            <img
+              className="h-7 "
+              src={
+                chainList.find((chain) => chain.name === selectedChain)?.logo ||
+                "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=029"
+              }
+              alt={selectedChain}
+            />
+            <ChevronDownIcon className="h-5" />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content className="flex flex-col bg-neutral-900 rounded-xl">
+            {chainList.map((chain) => (
+              <DropdownMenu.Item
+                key={chain.chainSelector}
+                onSelect={() =>
+                  handleChainSelect(chain.name, chain.chainSelector)
+                }
+                className="flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
+              >
+                <img className="h-7" src={chain.logo} alt={chain.name} />
+                <span>Pay from {chain.name}</span>
+              </DropdownMenu.Item>
+            ))}
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </div>
 
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger className="flex space-x-2 items-center pl-5 pr-2 bg-blue-500 border-l-2">
-          <img
-            className="h-7 items-center"
-            src={
-              selectedChain === "Polygon"
-                ? "https://polygontechnology.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F89203c58-0bc9-48b4-898c-18a637150354%2FGradient_on_Transparent.png?table=block&id=d51ab665-6522-4ba6-8480-feb2e18f1a27&spaceId=51562dc1-1dc5-4484-bf96-2aeac848ae2f&width=600&userId=&cache=v2"
-                : "/avalanche_logo.png"
-            }
-          />
-          <ChevronDownIcon className="h-5" />
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content className="flex flex-col bg-neutral-900 rounded-xl">
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Polygon", 80001)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://polygontechnology.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F89203c58-0bc9-48b4-898c-18a637150354%2FGradient_on_Transparent.png?table=block&id=d51ab665-6522-4ba6-8480-feb2e18f1a27&spaceId=51562dc1-1dc5-4484-bf96-2aeac848ae2f&width=600&userId=&cache=v2"
-              alt="Polygon Logo"
-            />
-            <span>Pay from Polygon</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Avalanche", 43113)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://cryptologos.cc/logos/avalanche-avax-logo.png?v=029"
-              alt="Avalanche Logo"
-            />
-            <span>Pay from Avalanche</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Avalanche", 43113)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://cryptologos.cc/logos/ethereum-eth-logo.png?v=026"
-              alt="Ethereum Logo"
-            />
-            <span>Coming Soon</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Avalanche", 43113)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://cryptologos.cc/logos/arbitrum-arb-logo.png?v=026"
-              alt="Arbitrum Logo"
-            />
-            <span>Coming Soon</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Avalanche", 43113)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://cryptologos.cc/logos/optimism-ethereum-op-logo.png?v=026"
-              alt="Optimism Logo"
-            />
-            <span>Coming Soon</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            onSelect={() => handleChainSelect("Avalanche", 43113)}
-            className=" flex items-center space-x-2 px-5 py-3 hover:bg-blue-500 cursor-pointer rounded-xl"
-          >
-            <img
-              className="h-7"
-              src="https://cryptologos.cc/logos/bnb-bnb-logo.png?v=026"
-              alt="BNB Smart Chain Logo"
-            />
-            <span>Coming Soon</span>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
+      <div className="flex items-center  py-2 mt-2">
+        <div className="w-full text-center">
+          {showRoutes && bestRoutes.length > 0 ? (
+            <p>
+              {bestRoutes.map((route, index) => (
+                <span key={index} className="text-sm">
+                  {route.name}
+                  {index < bestRoutes.length - 1 ? " \u2192 " : ""}
+                </span>
+              ))}
+            </p>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
     </>
   );
 }
