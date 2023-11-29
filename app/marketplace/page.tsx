3d0e71335e682ed800a6c94a1af2feef663c676e
keivnjs <kjsurtan@gmail.com>
2023-11-29 17:59:21 +0700

    fix: add force-dynamic

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,4 +1,4 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 import CustomImage from "./modules/Image";
 import { getNftsQuery } from "@/lib/gql/queries/nft";
