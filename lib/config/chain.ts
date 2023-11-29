2156dc8ff6371af5684b174962b235ff6f1878fb
keivnjs <kjsurtan@gmail.com>
2023-11-29 17:40:20 +0700

    fix: add marketplaceAddress

--- lib/config/chain.ts
+++ lib/config/chain.ts
@@ -6,6 +6,7 @@ export interface Chain {
   rpc: string;
   routerAddr: string;
   chainId: string;
+  marketplaceAddr: string;
 }
 
 export const chainList: Chain[] = [
@@ -17,6 +18,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0xD0daae2231E9CB96b94C8512223533293C3693Bf",
     chainId: "",
+    marketplaceAddr: "0x00D3eB554920E8069eAAD98E75D5B36173A8ee03",
   },
   {
     chainSelector: "2664363617261496610",
@@ -26,6 +28,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0xEB52E9Ae4A9Fb37172978642d4C141ef53876f26",
     chainId: "",
+    marketplaceAddr: "0x68b9bDD80727CC031d5Be9918E071b8e615c3C31",
   },
   {
     chainSelector: "14767482510784806043",
@@ -35,6 +38,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8",
     chainId: "",
+    marketplaceAddr: "0xADc0aF0dd4eef8Ac9176ACF5F10Cd18830ae8342",
   },
   {
     chainSelector: "6101244977088475029",
@@ -44,6 +48,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0x88E492127709447A5ABEFdaB8788a15B4567589E",
     chainId: "",
+    marketplaceAddr: "0x",
   },
   {
     chainSelector: "12532609583862916517",
@@ -53,6 +58,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0x70499c328e1E2a3c41108bd3730F6670a44595D1",
     chainId: "",
+    marketplaceAddr: "0x3b8a9c94c406f08Be7997136dF273FE652bc3612",
   },
   {
     chainSelector: "5790810961207155433",
@@ -62,6 +68,7 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0xa8c0c11bf64af62cdca6f93d3769b88bdd7cb93d",
     chainId: "",
+    marketplaceAddr: "0x9aDa66369E1f548aB048C7FC708b6271994a16D4",
   },
   {
     chainSelector: "13264668187771770619",
@@ -71,5 +78,6 @@ export const chainList: Chain[] = [
     rpc: "",
     routerAddr: "0x9527e2d01a3064ef6b50c1da1c0cc523803bcff2",
     chainId: "",
+    marketplaceAddr: "0xf3cef7fA414CB9a027f73a4d46f02092C5412862",
   },
 ];
