7730e0c0a0f0299af7531fde5e2a3e49b89f277a
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-12-01 00:33:34 +0700

    fix: updated the copywriting on the front page

--- app/page.tsx
+++ app/page.tsx
@@ -5,51 +5,46 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-left">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            How we build Crosslik?{" "}
-          </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>The Problem </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            <ul className="list-disc">
-              <li>
-                Chainlink CCIP: allows the seamless cross-chain transfer and
-                balance data syncing of these extended tokens used for payment.
-              </li>
-              <li>
-                The Graph: Index data such as the details of listed NFTs and
-                ease the process of showing data in the front end.
-              </li>
-              <li>
-                Djikstra&apos;s Algorithm: Built into an SDK. It helps finding
-                the best route between chains without direct lane and
-                interacting with smart contract.
-              </li>
-            </ul>
+            According to DefiLlama, there are 234 chains and with the
+            advancement of chain-creation tooling such as Arbitrum Orbit, OP
+            Stack, and Polygon CDK, we&apos;ll see more chains launched. dApps
+            will need to adapt to this and we believe Chainlink CCIP is the
+            answer.
+            <br />
+            <br />
+            From our firsthand experience building AnyApe for ETHOnline 2023, a
+            cross-chain NFT marketplace built with CCIP. We wouldn&apos;t say
+            the DX is bad per se, but it could be improved to ease developer in
+            building a cross-chain dApps.
           </p>
         </div>
 
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-center">
-          <h2 className={`mb-3 text-2xl font-semibold`}>What is Crosslink? </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Utilized Chainlink Services{" "}
+          </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Crosslink aids developers in integrating cross-chain functionality
-            into new or existing dApps, enabling seamless transactions across
-            different blockchains to improve user experience. We&apos;ll be
-            showcasing its utility in an NFT marketplace, where users could buy
-            NFTs on one chain using funds from another, exemplifying
-            Crosslink&apos;s versatility in practical applications.
+            <ul>
+              <li>- CRC1 & CRC20 uses Chainlink CCIP</li>
+              <li>- Fee Automation uses Chainlink Automations</li>
+            </ul>
           </p>
         </div>
 
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 lg:text-right">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Why we build Crosslink?{" "}
-          </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>The Solution</h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            We want to remove the frictions users experience when moving funds
-            across various chains. The most pressing issue is finding a bridge
-            protocol that supports both the source and destination chains,
-            aligns with the asset being transferred, and provides ample
-            liquidity for the transaction. Compounding these challenges are the
-            security risks.
+            OpenCCIP is a smart contract library abstracting CCIP functionality
+            designed to assist developers in creating cross-chain dApps.
+            <br />
+            <br />
+            OpenCCIP also keep security in mind and added various features such
+            as cross-chain data synching and cross-chain token handling. We also
+            created an SDK for finding the best lanes in processing transaction
+            across chains and easy integration between the front end and
+            OpenCCIP smart contracts.
           </p>
         </div>
       </div>
