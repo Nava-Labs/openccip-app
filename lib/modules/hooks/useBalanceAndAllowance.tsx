860ea64c13f68ddb405ceaf0acfa92c6094af3a5
keivnjs <kjsurtan@gmail.com>
2023-12-01 19:07:54 +0700

    feat: add hook useBalanceAndAllowance

---
+++ lib/modules/hooks/useBalanceAndAllowance.tsx
@@ -0,0 +1,43 @@
+import { useAccount, useContractReads, useNetwork } from "wagmi";
+import { ERC20_ABI } from "@/lib/abi/erc20-abi";
+
+export const useBalanceAndAllowance = (
+  token: `0x${string}`,
+  spender?: `0x${string}`
+) => {
+  const { address } = useAccount();
+
+  const { data, refetch } = useContractReads({
+    enabled: Boolean(address),
+    scopeKey: `${token}_balance_and_allowance`,
+    blockTag: "latest",
+    cacheOnBlock: true,
+    allowFailure: false,
+    contracts: [
+      {
+        address: token,
+        abi: ERC20_ABI,
+        functionName: "allowance",
+        args: [address!, spender as `0x${string}`],
+      } as const,
+      {
+        address: token,
+        abi: ERC20_ABI,
+        functionName: "balanceOf",
+        args: [address!],
+      } as const,
+    ],
+  });
+  if (!data)
+    return {
+      allowance: BigInt(0),
+      balance: BigInt(0),
+      refetch,
+    };
+
+  return {
+    allowance: data[0],
+    balance: data[1],
+    refetch,
+  };
+};
